0d9f61f03b788209b6d9482550fe9261457a7c3d
yomal148 <amaltharyan@gmail.com>
2022-09-11 18:23:27 -0400

    Adding toast message to button

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
@@ -3,6 +3,8 @@ package edu.northeastern.numad22fa_amaltharyan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toastMsg(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("Hello how are you today, my name is Amal and my email is tharyan.a@northeastern.edu");
     }
 }
