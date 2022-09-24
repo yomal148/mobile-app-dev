2cba19e3a769d3e97ed070a5aad0035e2591bc2c
yomal148 <amaltharyan@gmail.com>
2022-09-24 13:33:27 -0400

    Adding clicky clicky button and styling the button

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
@@ -2,8 +2,10 @@ package edu.northeastern.numad22fa_amaltharyan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.util.Log;
 
@@ -13,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // calling the super
         setContentView(R.layout.activity_main); // inflate the layout
+
+        Button click = (Button) findViewById(R.id.button2); //get reference to clicky clicky button
+        click.setBackgroundColor(Color.RED);
     }
 
     public void toastMsg(String msg){
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
         toast.show();
     }
+
 
     public void displayToastMsg(View v) {
         toastMsg("Hello, my name is Amal and my email is tharyan.a@northeastern.edu");
