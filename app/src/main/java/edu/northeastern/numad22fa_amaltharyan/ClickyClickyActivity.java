7df5144c966cecb751b8cf2efd28ffbb364865de
yomal148 <amaltharyan@gmail.com>
2022-09-24 17:01:52 -0400

    Clicky clicky button navigates to new activity

---
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/ClickyClickyActivity.java
@@ -0,0 +1,7 @@
+package edu.northeastern.numad22fa_amaltharyan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClickyClickyActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
@@ -2,12 +2,13 @@ package edu.northeastern.numad22fa_amaltharyan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState); // calling the super
         setContentView(R.layout.activity_main); // inflate the layout
 
-        Button click = (Button) findViewById(R.id.button2); //get reference to clicky clicky button
-        click.setBackgroundColor(Color.RED);
+        Button click = (Button) findViewById(R.id.clicky_clicky); // get reference to clicky clicky button
+        click.setBackgroundColor(Color.RED); // setting button background color to red
+
     }
 
+    // Displays a toast message when button is clicked
     public void toastMsg(String msg){
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
         toast.show();
     }
 
-
+    // Button click listener when button is clicked
     public void displayToastMsg(View v) {
         toastMsg("Hello, my name is Amal and my email is tharyan.a@northeastern.edu");
     }
+
+    // Utilize intent class to navigate to ClickyClicky class
+    public void gotoClickyClickyActivity(View v){
+        Intent intent = new Intent(MainActivity.this, ClickyClickyActivity.class);
+        startActivity(intent);
+    }
+
 }
