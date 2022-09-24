1110e63ecf8ff75637da6977ce10add63b19c646
yomal148 <amaltharyan@gmail.com>
2022-09-24 17:28:00 -0400

    Creating the buttons for clicky clicky activity

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/ClickyClickyActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/ClickyClickyActivity.java
@@ -2,6 +2,13 @@ package edu.northeastern.numad22fa_amaltharyan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class ClickyClickyActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+    }
 }
