c39f0d6f220e439af6fe85b45120b4841de84ebe
yomal148 <amaltharyan@gmail.com>
2022-09-25 16:17:16 -0400

    Making more changes to buttons layout

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/ClickyClickyActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/ClickyClickyActivity.java
@@ -3,12 +3,17 @@ package edu.northeastern.numad22fa_amaltharyan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ClickyClickyActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clicky_clicky);
+        super.onCreate(savedInstanceState); // calling the super
+        setContentView(R.layout.activity_clicky_clicky); // inflate the layout
+
+        TextView textView = (TextView)findViewById(R.id.pressed); // get reference to text view
+        textView.setText("Pressed:-"); // set initial text to Pressed:-
     }
 }
