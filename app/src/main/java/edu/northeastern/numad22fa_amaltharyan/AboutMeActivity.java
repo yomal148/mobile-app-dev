1f8180b52c5f5058b49045a8baac50b2ea4ba6d4
yomal148 <amaltharyan@gmail.com>
2022-09-29 18:24:12 -0400

    Adding new activity to about me button

---
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/AboutMeActivity.java
@@ -0,0 +1,24 @@
+package edu.northeastern.numad22fa_amaltharyan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class AboutMeActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+
+        name = (EditText) findViewById(R.id.editTextTextPersonName); // get reference to name text box
+        name.setText("Amal Tharyan"); // Setting text to my name
+
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress); // get reference to email text box
+        email.setText("tharyan.a@northeastern.edu"); // Setting text to email
+    }
+
+}
--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/MainActivity.java
@@ -7,30 +7,41 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button aboutMe;
+    private Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // calling the super
         setContentView(R.layout.activity_main); // inflate the layout
 
-        Button click = (Button) findViewById(R.id.clicky_clicky); // get reference to clicky clicky button
+        click = (Button) findViewById(R.id.clicky_clicky); // get reference to clicky clicky button
         click.setBackgroundColor(Color.RED); // setting button background color to red
 
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoClickyClickyActivity(view);
+            }
+        });
+
+        aboutMe = (Button) findViewById(R.id.about_me); // get reference to about me button
+        aboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoAboutMeActivity(view);
+            }
+        });
     }
 
-    // Displays a toast message when button is clicked
-    public void toastMsg(String msg){
-        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
-        toast.show();
-    }
 
-    // Button click listener when button is clicked
-    public void displayToastMsg(View v) {
-        toastMsg("Hello, my name is Amal and my email is tharyan.a@northeastern.edu");
+    // navigate to About me activity
+    public void gotoAboutMeActivity(View v){
+        Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+        startActivity(intent);
     }
 
     // Utilize intent class to navigate to ClickyClicky class
