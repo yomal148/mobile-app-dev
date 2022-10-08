d41b5b44e7851fb871c6aa24acd43d8a4633bad6
yomal148 <amaltharyan@gmail.com>
2022-10-08 16:36:01 -0400

    Fixing terminating search and add verification to back button press

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/PrimeDirectiveActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/PrimeDirectiveActivity.java
@@ -2,6 +2,8 @@ package edu.northeastern.numad22fa_amaltharyan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,47 +16,21 @@ public class PrimeDirectiveActivity extends AppCompatActivity {
     private Button terminateSearch;
     private TextView text1;
     private TextView text2;
+    private Handler textHandler = new Handler();
 
-    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_directive);
 
-        Handler textHandler = new Handler();
         findPrimes = (Button) findViewById(R.id.find_primes);
         terminateSearch = (Button) findViewById(R.id.terminate_search);
 
         text1 = findViewById(R.id.currentNumber);
         text2 = findViewById(R.id.currentNumber2);
 
-
-        final Thread th = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!running) {
-                    for (int i = 3; ; i += 2) {
-                        final int finalI = i;
-                        textHandler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                text1.setText("Current number: " + finalI);
-                                if (isPrime(finalI)) {
-                                    text2.setText("Latest prime found: " + finalI);
-                                }
-                            }
-                        });
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
-
+        final myThread th = new myThread();
 
         findPrimes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,15 +39,49 @@ public class PrimeDirectiveActivity extends AppCompatActivity {
             }
         });
 
+
         terminateSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                running = true;
-                Toast.makeText(PrimeDirectiveActivity.this, "Terminated", Toast.LENGTH_LONG).show();
+               th.terminate();
             }
         });
 
     }
+
+    class myThread extends Thread{
+
+        private volatile boolean running = true;
+        //private volatile boolean infiniteRun = true;
+        public void terminate(){
+            running = false;
+        }
+
+        @Override
+        public void run() {
+            while(running) {
+                for (int i = 3; ; i += 2) {
+                    if(running == false){
+                        break;
+                    }
+                    final int finalI = i;
+                    textHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text1.setText("Current number: " + finalI);
+                            if (isPrime(finalI)) {
+                                text2.setText("Latest prime found: " + finalI);
+                            }
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
         public boolean isPrime(int n) {
             if (n == 1) {
@@ -86,6 +96,26 @@ public class PrimeDirectiveActivity extends AppCompatActivity {
             }
             return true;
         }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("User Verification")
+                .setMessage("Do you want to close this activity?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
 
 }
 
