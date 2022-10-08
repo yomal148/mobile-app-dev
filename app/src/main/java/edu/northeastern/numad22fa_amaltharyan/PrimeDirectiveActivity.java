21afff194df8c4b64e0d96b848b7de2d36ba5e47
yomal148 <amaltharyan@gmail.com>
2022-10-07 21:31:13 -0400

    working search lookup and prime number find thread

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/PrimeDirectiveActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/PrimeDirectiveActivity.java
@@ -3,14 +3,80 @@ package edu.northeastern.numad22fa_amaltharyan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
 
 public class PrimeDirectiveActivity extends AppCompatActivity {
+    private Button findPrimes;
+    private Button terminateSearch;
+    private TextView text1;
+    private TextView text2;
 
+    //myThread t = new myThread();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_directive);
+
+        Handler textHandler = new Handler();
+        findPrimes = (Button) findViewById(R.id.find_primes);
+        terminateSearch = (Button) findViewById(R.id.terminate_search);
+
+        text1 = findViewById(R.id.currentNumber);
+        text2 = findViewById(R.id.currentNumber2);
+
+        findPrimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread th = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 3; ; i += 2) {
+                            final int finalI = i;
+                            textHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    text1.setText("Current number: " + finalI);
+                                    if (isPrime(finalI)){
+                                        text2.setText("Latest prime found: " + finalI);
+                                    }
+                                }
+                            });
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                th.start();
+            }
+        });
+
+        terminateSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
-    
+        public boolean isPrime(int n) {
+            if (n == 1) {
+                return false;
+            }
+
+            // looping from 2 to half of the number and checking if its divisible by those numbers
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
+
