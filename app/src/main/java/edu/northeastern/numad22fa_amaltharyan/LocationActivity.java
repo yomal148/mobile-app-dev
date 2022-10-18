3a28e274680377245068d99d13d621b8eb1173ed
yomal148 <amaltharyan@gmail.com>
2022-10-18 22:23:08 +0400

    adding total distance calculation

--- app/src/main/java/edu/northeastern/numad22fa_amaltharyan/LocationActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_amaltharyan/LocationActivity.java
@@ -10,13 +10,17 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity implements LocationListener {
     private TextView locationText;
+    private TextView distanceTravelled;
     private LocationManager locationManager;
+    private Location last;
+    private float distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +30,17 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         locationText = (TextView)findViewById(R.id.location_text);
         locationManager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
 
+        distanceTravelled = (TextView)findViewById(R.id.distance_travelled);
         if (ActivityCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) !=
                 PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( this,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
-        locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, this );
+
+        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 5, this );
         onLocationChanged(location);
+
     }
 
 
@@ -43,6 +50,17 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
 
-        locationText.setText("Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+        locationText.setText("Current Location" + "\n\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+        locationText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22f);
+
+        if(last != null){
+            distance += location.distanceTo(last);
+        }
+
+        last = new Location(location);
+
+        distanceTravelled.setText("Total distance: " + distance + " meters");
     }
+
+
 }
